1badf7c5ad2d267d58ca1cb084e763a08cb2d480
Nikolay Pavlov <nikolay.pavlov@protonmail.com>
2017-01-03 22:23:34 +0200

    Resolved conflicts

--- src/app/houses/components/house-list/house-list.component.ts
+++ src/app/houses/components/house-list/house-list.component.ts
@@ -4,14 +4,38 @@ import { Observable } from 'rxjs/Observable';
 import { HouseService } from '../../services';
 import { House } from '../../models';
 
+import { Modal } from 'ng2-modal';
+
+
 @Component({
   selector: 'house',
   templateUrl: './house-list.component.html',
 })
 export class HouseListComponent {
-  userHouses: Observable<House[]>;
+  userHouses: House[];
+  removeHouseModal: Modal;
+  selectedHouseForRemoval: House;
 
   constructor(private houseService: HouseService) {
-    this.userHouses = this.houseService.getUserHouses();
+    this.houseService.getUserHouses()
+      .subscribe((houses: House[]) => {
+        this.userHouses = houses;
+      });
+  }
+
+  onHouseRemoveButtonClick(house: House) {
+    this.selectedHouseForRemoval = house;
+    this.removeHouseModal.open();
+  }
+
+  onModalHouseRemoveInit(removeHouseModal: Modal) {
+    this.removeHouseModal = removeHouseModal;
+  }
+
+  onModalHouseRemoveButtonPressed() {
+    this.houseService.delete(this.selectedHouseForRemoval)
+      .then((result) => {
+        this.removeHouseModal.close();
+      });
   }
 }
